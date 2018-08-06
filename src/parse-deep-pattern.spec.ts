1b5729ac03bfe0585ced0c5f7623709562681bb3
Jakub Barczyk <jakubbarczyk@yahoo.com>
2018-08-06 19:34:24 +0200

    Clean up test specs

--- src/parse-deep-pattern.spec.ts
+++ src/parse-deep-pattern.spec.ts
@@ -1,17 +1,15 @@
 import { parseDeepPattern } from "./parse-deep-pattern";
 
-describe("Mocktail Expression Language", () => {
-    describe("parseDeepPattern", () => {
-        it("should mock shallow object off of provided pattern", () => {
-            expect(parseDeepPattern("foo", 123)).toEqual({ foo: 123 });
-        });
+describe("parseDeepPattern", () => {
+    it("should mock shallow object off of provided pattern", () => {
+        expect(parseDeepPattern("foo", 123)).toEqual({ foo: 123 });
+    });
 
-        it("should mock deep object off of provided pattern", () => {
-            expect(parseDeepPattern("foo.bar", null)).toEqual({ foo: { bar: null } });
-        });
+    it("should mock deep object off of provided pattern", () => {
+        expect(parseDeepPattern("foo.bar", null)).toEqual({ foo: { bar: null } });
+    });
 
-        it("should not require passing nested value argument", () => {
-            expect(parseDeepPattern("foo.bar.baz")).toEqual({ foo: { bar: { baz: undefined } } });
-        });
+    it("should not require passing nested value argument", () => {
+        expect(parseDeepPattern("foo.bar.baz")).toEqual({ foo: { bar: { baz: undefined } } });
     });
 });
--- src/parse-wide-pattern.spec.ts
+++ src/parse-wide-pattern.spec.ts
@@ -1,17 +1,15 @@
 import { parseWidePattern } from "./parse-wide-pattern";
 
-describe("Mocktail Expression Language", () => {
-    describe("parseWidePatterns", () => {
-        it("should parse a pattern with several wides", () => {
-            expect(parseWidePattern("foo.bar,baz,boo")).toEqual(["foo.bar", "baz", "boo"]);
-        });
+describe("parseWidePatterns", () => {
+    it("should parse a pattern with several wides", () => {
+        expect(parseWidePattern("foo.bar,baz,boo")).toEqual(["foo.bar", "baz", "boo"]);
+    });
 
-        it("should parse a pattern with a single wide", () => {
-            expect(parseWidePattern("foo.bar")).toEqual(["foo.bar"]);
-        });
+    it("should parse a pattern with a single wide", () => {
+        expect(parseWidePattern("foo.bar")).toEqual(["foo.bar"]);
+    });
 
-        it("should parse a pattern without wides", () => {
-            expect(parseWidePattern("")).toEqual([""]);
-        });
+    it("should parse a pattern without wides", () => {
+        expect(parseWidePattern("")).toEqual([""]);
     });
 });
